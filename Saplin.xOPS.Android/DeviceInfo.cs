52e9aff3b36fbc40700ecc1e11f6f9cba3fba6a0
sdfsfd <sdfd@sdfsd.ff>
2020-05-07 18:47:55 +0300

    1.1.0

--- Saplin.xOPS.Android/DeviceInfo.cs
+++ Saplin.xOPS.Android/DeviceInfo.cs
@@ -25,28 +25,39 @@ namespace Saplin.xOPS.Droid
             var paths = new string[] {
                 "/sys/devices/system/cpu/cpu0/cpufreq/cpu_temp",
                 "/sys/devices/system/cpu/cpu0/cpufreq/FakeShmoo_cpu_temp",
-                "/sys/class/thermal/thermal_zone0/temp",
-                "/sys/class/thermal/thermal_zone1/temp",
-                "/sys/class/i2c-adapter/i2c-4/4-004c/temperature",
-                "/sys/devices/platform/tegra-i2c.3/i2c-4/4-004c/temperature",
-                "/sys/devices/platform/omap/omap_temp_sensor.0/temperature",
-                "/sys/devices/platform/tegra_tmon/temp1_input",
-                "/sys/kernel/debug/tegra_thermal/temp_tj",
-                "/sys/devices/platform/s5p-tmu/temperature",
-                "/sys/class/thermal/thermal_zone1/temp",
-                "/sys/class/hwmon/hwmon0/device/temp1_input",
+
                 "/sys/devices/virtual/thermal/thermal_zone1/temp",
                 "/sys/devices/virtual/thermal/thermal_zone0/temp",
-                "/sys/class/thermal/thermal_zone3/temp",
-                "/sys/class/thermal/thermal_zone4/temp",
-                "/sys/class/hwmon/hwmon0/temp1_input",
-                "/sys/class/hwmon/hwmonX/temp1_input",
+                "/sys/devices/virtual/hwmon/hwmon1/temp1_input", //Nokia N1, sensor name in 'sensor'
+
+                "/sys/devices/platform/omap/omap_temp_sensor.0/temperature",
+
+                "/sys/devices/platform/tegra-i2c.3/i2c-4/4-004c/temperature", 
+                "/sys/devices/platform/tegra_tmon/temp1_input",
+                "/sys/devices/platform/tegra-i2c.3/i2c-4/4-004c/ext_temperature",
+                "/sys/devices/platform/tegra-tsensor/tsensor_temperature",
+
+                "/sys/devices/platform/s5p-tmu/temperature",
                 "/sys/devices/platform/s5p-tmu/curr_temp",
                 "/sys/devices/platform/s5p-tmu/temperature",
+
+                // /sys/devices/virtual/thermal/thermal_zone1/temp  - miui.eu custom rom
+
+                "/sys/class/thermal/thermal_zone0/temp",
+                "/sys/class/thermal/thermal_zone1/temp",
+                "/sys/class/thermal/thermal_zone3/temp",
+                "/sys/class/thermal/thermal_zone4/temp",
+
+                "/sys/class/i2c-adapter/i2c-4/4-004c/temperature",
+
+                "/sys/class/hwmon/hwmon0/temp1_input",
+                "/sys/class/hwmon/hwmonX/temp1_input", 
+                "/sys/class/hwmon/hwmon0/device/temp1_input",
+
                 "/sys/kernel/debug/tegra_thermal/temp_tj",
-                "/sys/htc/cpu_temp",
-                "/sys/devices/platform/tegra-i2c.3/i2c-4/4-004c/ext_temperature",
-                "/sys/devices/platform/tegra-tsensor/tsensor_temperature"};
+               
+                "/sys/htc/cpu_temp"
+            };
 
             foreach (var p in paths)
             {
@@ -55,7 +66,7 @@ namespace Saplin.xOPS.Droid
                     cpuTempFile = new RandomAccessFile(p, "r");
                     //cpuTempFile.Seek(0);
                     //var s = cpuTempFile?.ReadLine();
-                    //break;
+                    break;
                 }
                 catch { }
             }
