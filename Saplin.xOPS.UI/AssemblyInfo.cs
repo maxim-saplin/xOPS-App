5ee96505cadad1d03d89386b200b75138a0d447e
sdfsfd <sdfd@sdfsd.ff>
2020-05-17 20:34:42 +0300

    1.1.1 - XF 4.6 SR1 (WebView fix for Android)

--- Saplin.xOPS.UI/AssemblyInfo.cs
+++ Saplin.xOPS.UI/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-//[assembly: ExportFont("SourceCodePro-Regular.ttf", Alias = "SourceCodePro")]
+[assembly: ExportFont("SourceCodePro-Regular.ttf", Alias = "SourceCodePro")]
