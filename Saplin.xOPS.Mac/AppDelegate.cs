0cd23ab0d9851d7d5dfcae5332f061155ce7927a
Maxim Saplin <smaxmail@gmail.com>
2022-06-16 22:20:03 +0300

    mac Universal binary, closable windows

--- Saplin.xOPS.Mac/AppDelegate.cs
+++ Saplin.xOPS.Mac/AppDelegate.cs
@@ -12,18 +12,16 @@ namespace Saplin.xOPS.Mac
         NSWindow _window;
         public AppDelegate()
         {
-            var style = NSWindowStyle.Resizable | NSWindowStyle.Titled | NSWindowStyle.FullSizeContentView;
+            var style = NSWindowStyle.Resizable | NSWindowStyle.Titled | NSWindowStyle.FullSizeContentView | NSWindowStyle.Closable;
 
             var rect = new CoreGraphics.CGRect(200, 1000, 500, 650);
             _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
             _window.Title = "xOPS";
-            _window.StandardWindowButton(NSWindowButton.CloseButton).Enabled = true;
-            _window.WindowShouldClose += WindowShouldClose;
-            //_window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+    
             _window.TitlebarAppearsTransparent = true;
-            //_window.StandardWindowButton(NSWindowButton.CloseButton).Hidden = true;
             _window.StandardWindowButton(NSWindowButton.ZoomButton).Hidden = true;
             _window.StandardWindowButton(NSWindowButton.MiniaturizeButton).Hidden = true;
+            _window.WindowShouldClose += WindowShouldClose;
             _window.MakeKeyWindow();
         }
 
